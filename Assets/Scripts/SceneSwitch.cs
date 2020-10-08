08d1e08c8af961f101bf111b21cea4adf3960882
Synral <44206227+Synral@users.noreply.github.com>
2020-10-08 18:19:16 -0400

    [Add] SceneSwitcher
    +Added scene switching
    +Implemented buttons for switching

---
+++ Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    public void enterMenuScene()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+    public void enterPlayScene()
+    {
+        SceneManager.LoadScene("PlayScene");
+    }
+    public void enterScoreScene()
+    {
+        SceneManager.LoadScene("ScoreScene");
+    }
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
