788fc2272dafb8f0565bfc8e234dcf4f9d0f0414
Synral <44206227+Synral@users.noreply.github.com>
2020-10-09 07:54:13 -0400

    [Add] Native C++
    +Added source code for DLL (DLL was added to Unity on repo creation)

---
+++ Native C++/Midterm/Midterm/Wrapper.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "CheckpointTimeLogger.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+	//Put your functions here
+	PLUGIN_API void ResetLogger();
+	PLUGIN_API void SaveCheckpointTime(float RTBC);
+	PLUGIN_API float GetTotalTime();
+	PLUGIN_API float GetCheckpointTime(int index);
+	PLUGIN_API int GetNumCheckpoints();
+
+#ifdef __cplusplus
+}
+#endif
