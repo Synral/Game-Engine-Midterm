c7ec803d575ec22cdb4a4cbad5592cb97522ee31
Synral <44206227+Synral@users.noreply.github.com>
2020-10-09 07:49:56 -0400

    [Add] Movement and Camera Systems
    +CameraMovement: Camera follows mouse for panning
    +PlayerMovement: Moves in direction camera points to

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform player;
+    float cameraX = 0.0f;
+    float cameraY = 0.0f;
+    float mouseSens = 1000.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotationX = Input.GetAxis("Mouse X") * Time.deltaTime * mouseSens;
+        float rotationY = Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSens;
+        cameraX -=rotationY;
+        cameraX = Mathf.Clamp(cameraX, -90.0f, 90.0f);
+
+        cameraY += rotationX;
+        
+        transform.localRotation = Quaternion.Euler(cameraX, cameraY, 0.0f);
+        player.Rotate(Vector3.up * rotationX);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController player;
+    public GameObject cam;
+    private Vector3 playerVelocity;
+    private float moveSpeed = 15.0f;
+    private float jumpSpeed = 3.0f;
+    private float gravity = -9.81f;
+    private bool onGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        onGround = player.isGrounded;
+        if (onGround && playerVelocity.y < 0.0f)
+            playerVelocity.y = -2.0f;
+        if (Input.GetKeyDown(KeyCode.Space) && onGround)
+            playerVelocity.y = Mathf.Sqrt(jumpSpeed * -2.0f * gravity);
+        if (Input.GetKeyDown(KeyCode.LeftShift) && onGround)
+            moveSpeed *= 1.2f;
+        float inputX = Input.GetAxis("Horizontal");
+        float inputZ = Input.GetAxis("Vertical");
+        Vector3 move = player.transform.right * inputX + player.transform.forward * inputZ;
+        move = cam.transform.rotation * move;
+        player.Move(move * Time.deltaTime * moveSpeed);
+
+        playerVelocity.y += gravity * Time.deltaTime;
+        player.Move(playerVelocity * Time.deltaTime * jumpSpeed);
+    }
+}
